4b4e892bbab45a7bec53a8d1b1a79dd0901d82cf
DanoRiv <danio260496@gmail.com>
2024-08-25 17:57:14 -0500

    HU-01 Refactor validations in the driving adapters

--- src/main/java/com/pragma_bootcamp/stock_service/adapters/driving/http/controller/CategoryControllerAdapter.java
+++ src/main/java/com/pragma_bootcamp/stock_service/adapters/driving/http/controller/CategoryControllerAdapter.java
@@ -3,6 +3,7 @@ package com.pragma_bootcamp.stock_service.adapters.driving.http.controller;
 import com.pragma_bootcamp.stock_service.adapters.driving.http.dto.request.CategoryRequest;
 import com.pragma_bootcamp.stock_service.adapters.driving.http.mapper.ICategoryRequestMapper;
 import com.pragma_bootcamp.stock_service.domain.api.ICategoryServicePort;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ public class CategoryControllerAdapter {
     private final ICategoryServicePort categoryServicePort;
     private final ICategoryRequestMapper categoryRequestMapper;
     @PostMapping("/")
-    public ResponseEntity<Void> addCategory(@RequestBody CategoryRequest categoryRequest) {
+    public ResponseEntity<Void> addCategory(@Valid @RequestBody CategoryRequest categoryRequest) {
         categoryServicePort.saveCategory(categoryRequestMapper.toCategory(categoryRequest));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
--- src/main/java/com/pragma_bootcamp/stock_service/adapters/driving/http/dto/response/CategoryResponse.java
+++ src/main/java/com/pragma_bootcamp/stock_service/adapters/driving/http/dto/response/CategoryResponse.java
@@ -2,8 +2,10 @@ package com.pragma_bootcamp.stock_service.adapters.driving.http.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class CategoryResponse {
     private Long id;
