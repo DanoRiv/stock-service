a25feca4a8411631645a12502c00f7574c42f032
DanoRiv <danio260496@gmail.com>
2024-08-25 22:34:28 -0500

    Remove NoArgsConstructor since it caused get to bring null results

--- src/main/java/com/pragma_bootcamp/stock_service/adapters/driving/http/dto/response/CategoryResponse.java
+++ src/main/java/com/pragma_bootcamp/stock_service/adapters/driving/http/dto/response/CategoryResponse.java
@@ -2,10 +2,8 @@ package com.pragma_bootcamp.stock_service.adapters.driving.http.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class CategoryResponse {
     private Long id;
