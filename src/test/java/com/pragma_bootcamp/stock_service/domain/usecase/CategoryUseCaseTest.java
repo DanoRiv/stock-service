6e3b61b8aa3d3315f2e8bac4bece68fb6b680caa
DanoRiv <danio260496@gmail.com>
2024-08-29 23:03:01 -0500

    Test/Add tests for pagination

--- src/test/java/com/pragma_bootcamp/stock_service/domain/usecase/CategoryUseCaseTest.java
+++ src/test/java/com/pragma_bootcamp/stock_service/domain/usecase/CategoryUseCaseTest.java
@@ -4,14 +4,17 @@ import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.exception.Dup
 import com.pragma_bootcamp.stock_service.domain.exception.EmptyFieldException;
 import com.pragma_bootcamp.stock_service.domain.model.Category;
 import com.pragma_bootcamp.stock_service.domain.spi.ICategoryPersistencePort;
+import com.pragma_bootcamp.stock_service.domain.util.PaginatedResult;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -61,5 +64,36 @@ class CategoryUseCaseTest {
 
         // Act & Assert
         assertThrows(DuplicatedEntryException.class, () -> categoryUseCase.saveCategory(category));
+    }
+
+    @Test
+    void shouldReturnPaginatedResultOfAllCategories(){
+
+        //arrange
+        Category category1 = new Category(1L, "Books", "Books category");
+        Category category2 = new Category(2L, "Electronics", "Electronics category");
+        List<Category> content = Arrays.asList(category1, category2);
+
+        long totalElements = 2;
+        int totalPages = 1;
+        int size = 2;
+        int page = 0;
+
+        PaginatedResult<Category> expectedResult = new PaginatedResult<>(content, page, size, totalElements, totalPages);
+
+        when(categoryPersistencePort.getAllCategories(0, 2, "asc")).thenReturn(expectedResult);
+
+        //act
+        PaginatedResult<Category> actualResult = categoryUseCase.getAllCategories(0, 2, "asc");
+        //assert
+        assertNotNull(actualResult);
+        assertEquals(expectedResult, actualResult);
+        assertEquals(content, actualResult.getContent());
+        assertEquals(totalElements, actualResult.getTotalElements());
+        assertEquals(totalPages, actualResult.getTotalPages());
+        assertEquals(page, actualResult.getPage());
+        assertEquals(size, actualResult.getSize());
+
+        verify(categoryPersistencePort, times(1)).getAllCategories(0, 2, "asc");
     }
 }
