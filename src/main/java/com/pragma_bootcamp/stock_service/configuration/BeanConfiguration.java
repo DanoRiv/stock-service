51bdfc75cca1693685ecb4baaa6c436b764f1792
DanoRiv <danio260496@gmail.com>
2024-08-30 17:09:20 -0500

    Feature/Add Brand beans to configuration file

--- src/main/java/com/pragma_bootcamp/stock_service/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma_bootcamp/stock_service/configuration/BeanConfiguration.java
@@ -1,10 +1,16 @@
 package com.pragma_bootcamp.stock_service.configuration;
 
+import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.adapter.BrandAdapter;
 import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.adapter.CategoryAdapter;
+import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.mapper.IBrandEntityMapper;
 import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.mapper.ICategoryEntityMapper;
+import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.repository.IBrandRepository;
 import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.repository.ICategoryRepository;
+import com.pragma_bootcamp.stock_service.domain.api.IBrandServicePort;
 import com.pragma_bootcamp.stock_service.domain.api.ICategoryServicePort;
+import com.pragma_bootcamp.stock_service.domain.spi.IBrandPersistencePort;
 import com.pragma_bootcamp.stock_service.domain.spi.ICategoryPersistencePort;
+import com.pragma_bootcamp.stock_service.domain.usecase.BrandUseCase;
 import com.pragma_bootcamp.stock_service.domain.usecase.CategoryUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +21,8 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
     private final ICategoryEntityMapper categoryEntityMapper;
     private final ICategoryRepository categoryRepository;
+    private final IBrandEntityMapper brandEntityMapper;
+    private final IBrandRepository brandRepository;
 
     @Bean
     public ICategoryPersistencePort categoryPersistencePort() {
@@ -24,5 +32,14 @@ public class BeanConfiguration {
     @Bean
     public ICategoryServicePort categoryServicePort() {
         return new CategoryUseCase(categoryPersistencePort());
+    }
+
+    @Bean
+    public IBrandPersistencePort brandPersistencePort(){
+        return new BrandAdapter(brandRepository, brandEntityMapper);
+    }
+    @Bean
+    public IBrandServicePort brandServicePort (){
+        return new BrandUseCase(brandPersistencePort());
     }
 }
