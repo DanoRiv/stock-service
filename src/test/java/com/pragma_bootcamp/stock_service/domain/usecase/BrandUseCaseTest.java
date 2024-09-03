7f44214038b4f87c8b8eb115453ed50a26f5d8cd
DanoRiv <danio260496@gmail.com>
2024-09-03 17:16:23 -0500

    Test/Add tests for domain layer

--- src/test/java/com/pragma_bootcamp/stock_service/domain/usecase/BrandUseCaseTest.java
+++ src/test/java/com/pragma_bootcamp/stock_service/domain/usecase/BrandUseCaseTest.java
@@ -4,14 +4,17 @@ import com.pragma_bootcamp.stock_service.domain.exception.DuplicatedEntryExcepti
 import com.pragma_bootcamp.stock_service.domain.exception.EmptyFieldException;
 import com.pragma_bootcamp.stock_service.domain.model.Brand;
 import com.pragma_bootcamp.stock_service.domain.spi.IBrandPersistencePort;
+import com.pragma_bootcamp.stock_service.domain.util.PaginatedResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -60,5 +63,36 @@ class BrandUseCaseTest {
 
         // Act & Assert
         assertThrows(DuplicatedEntryException.class, () -> brandUseCase.saveBrand(brand));
+    }
+
+    @Test
+    void shouldReturnPaginatedResultOfAllBrands(){
+
+        //arrange
+        Brand brand1 = new Brand(1L, "Coca cola", "Soda brand");
+        Brand brand2 = new Brand(2L, "Renault", "Cars brand");
+        List<Brand> content = Arrays.asList(brand1, brand2);
+
+        long totalElements = 2;
+        int totalPages = 1;
+        int size = 2;
+        int page = 0;
+
+        PaginatedResult<Brand> expectedResult = new PaginatedResult<>(content, page, size, totalElements, totalPages);
+
+        when(brandPersistencePort.getAllBrands(0, 2, "asc")).thenReturn(expectedResult);
+
+        //act
+        PaginatedResult<Brand> actualResult = brandUseCase.getAllBrands(0, 2, "asc");
+        //assert
+        assertNotNull(actualResult);
+        assertEquals(expectedResult, actualResult);
+        assertEquals(content, actualResult.getContent());
+        assertEquals(totalElements, actualResult.getTotalElements());
+        assertEquals(totalPages, actualResult.getTotalPages());
+        assertEquals(page, actualResult.getPage());
+        assertEquals(size, actualResult.getSize());
+
+        verify(brandPersistencePort, times(1)).getAllBrands(0, 2, "asc");
     }
 }
