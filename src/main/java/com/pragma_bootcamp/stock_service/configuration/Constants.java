5cd5ee289bd1a9a94d4a2770ab6f63f15c8ee737
DanoRiv <danio260496@gmail.com>
2024-09-12 21:04:35 -0500

    Feature/Add constants on brand and category

--- src/main/java/com/pragma_bootcamp/stock_service/configuration/Constants.java
+++ src/main/java/com/pragma_bootcamp/stock_service/configuration/Constants.java
@@ -8,4 +8,13 @@ public class Constants {
     public static final String NO_DATA_FOUND_EXCEPTION_MESSAGE = "No results found in the database";
     public static final String EMPTY_FIELD_EXCEPTION_MESSAGE = "The field cannot be empty";
     public static final String SORT_BY_NAME = "name";
+
+
+    public static final int MAX_CHARACTERS_CATEGORY_NAME = 50;
+    public static final int MAX_CHARACTERS_CATEGORY_DESCRIPTION= 90;
+    public static final int MAX_CHARACTERS_BRAND_NAME = 50;
+    public static final int MAX_CHARACTERS_BRAND_DESCRIPTION = 120;
+    public static final int MIN_CATEGORIES_PER_ITEM = 1;
+    public static final int MAX_CATEGORIES_PER_ITEM = 3;
+
 }
