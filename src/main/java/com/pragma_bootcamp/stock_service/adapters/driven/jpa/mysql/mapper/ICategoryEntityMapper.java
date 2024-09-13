8109c56b932646bee95c3e69907757804ac83f4f
DanoRiv <danio260496@gmail.com>
2024-09-12 21:00:27 -0500

    Feature/Modify mappers to associate them with items

--- src/main/java/com/pragma_bootcamp/stock_service/adapters/driven/jpa/mysql/mapper/ICategoryEntityMapper.java
+++ src/main/java/com/pragma_bootcamp/stock_service/adapters/driven/jpa/mysql/mapper/ICategoryEntityMapper.java
@@ -3,12 +3,18 @@ package com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.mapper;
 import com.pragma_bootcamp.stock_service.adapters.driven.jpa.mysql.entity.CategoryEntity;
 import com.pragma_bootcamp.stock_service.domain.model.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface ICategoryEntityMapper {
     Category toModel(CategoryEntity categoryEntity);
     List<Category> toModelList(List<CategoryEntity> categoryEntities);
+    @Mapping(target = "items", ignore = true)
     CategoryEntity toEntity(Category category);
+    default Optional<Category> toCategoryOptional(Optional<CategoryEntity> categoryEntity){
+        return categoryEntity.map(this::toModel);
+    }
 }
